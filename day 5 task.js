9e05c4dacffc0e7120ec5b38d2dea78236ad3f03
DHEEPAK S <131872629+dheepak2000s@users.noreply.github.com>
2023-05-07 18:33:04 +0530

    Add files via upload

---
+++ day 5 task.js
@@ -0,0 +1,45 @@
+let myResume={
+    "basics": {
+      "name": "DHEEPAK S",
+      "email": "dheepak2000s@gmail.com",
+      "phone": 6383178550,
+      "degree": "B.E",
+      "location": {
+        "address": "no-227,sadayampatti post,ponnamaravathy tk",
+        "postalCode": 622002,
+        "city": "Pudukkottai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/dheepak-s-835751226/",
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "K Ramakrishnan college of engineering",
+        "department": "ECE",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "gpa": 7.08,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        "project": [
+          "IOt based river filtering system"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+  }
+  console.log(myResume);
